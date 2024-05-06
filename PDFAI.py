56e6b370755e5b8c45f1d487ea62912b95cc2e3b
Thabo Maduna <Thabo.Maduna@fnb.co.za>
2024-05-06 13:54:14 +0200

    project clean up

--- PDFAI.py
+++ PDFAI.py
@@ -3,7 +3,7 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain_community.embeddings import HuggingFaceInstructEmbeddings
-from langchain_community.vectorstores import FAISS
+from langchain.vectorstores import FAISS
 from langchain_community.llms import HuggingFacePipeline
 
 def get_pdf_text(pdf_docs):
@@ -17,8 +17,8 @@ def get_pdf_text(pdf_docs):
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
+        chunk_size=500,
+        chunk_overlap=50,
         length_function=len
     )
     chunks = text_splitter.split_text(text)
@@ -32,9 +32,8 @@ def get_vectorstore(text_chunks):
 
 def get_conversation_chain(vectorstore):
     # Load the local model components
-    model_name = "./pretrainedModels/flan-t5-large"
-    # model_name = "google/flan-t5-small"
-    llm = HuggingFacePipeline.from_model_id(model_id= model_name, task="text2text-generation", model_kwargs={"temperature": 0.1, "max_length": 200})
+    model_name = "./pretrainedModels/flan-t5-small"
+    llm = HuggingFacePipeline.from_model_id(model_id= model_name, task="text2text-generation", model_kwargs={"temperature": 0.1, "max_length": 1000, "do_sample": True})
     conversation_chain = RetrievalQA.from_chain_type(llm, retriever=vectorstore.as_retriever())
     return conversation_chain
 
